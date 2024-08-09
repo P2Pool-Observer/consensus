e92f61bc6792929b19ed3938da93930b4c0381e4
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2024-08-09 21:22:35 +0200

    Version v4.0.5

--- monero/address/packed_base58.go
+++ monero/address/packed_base58.go
@@ -2,27 +2,15 @@
 
 package address
 
-import (
-	"git.gammaspectra.live/P2Pool/consensus/v4/monero/crypto"
-	base58 "git.gammaspectra.live/P2Pool/monero-base58"
-)
-
 func (p PackedAddress) String() string {
 	return string(p.ToBase58(PackedAddressGlobalNetwork))
 }
 
 func (p PackedAddress) MarshalJSON() ([]byte, error) {
-	var nice [69]byte
-	nice[0] = PackedAddressGlobalNetwork
-	copy(nice[1:], p[PackedAddressSpend][:])
-	copy(nice[1+crypto.PublicKeySize:], p[PackedAddressView][:])
-	sum := crypto.PooledKeccak256(nice[:65])
-
 	buf := make([]byte, 0, 97)
-	buf2 := base58.EncodeMoneroBase58PreAllocated(buf[1:], []byte{PackedAddressGlobalNetwork}, p[PackedAddressSpend][:], p[PackedAddressView][:], sum[:4])
-	buf = buf[:len(buf2)+1]
-	buf[0] = '"'
-	buf[len(buf)-1] = '"'
+	buf = append(buf, '"')
+	buf = append(buf, p.ToBase58(PackedAddressGlobalNetwork)...)
+	buf = append(buf, '"')
 	return buf, nil
 }
 
