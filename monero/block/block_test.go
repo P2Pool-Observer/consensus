49d991bdc1acb6692f187e88ff301391adb54443
WeebDataHoarder <weebdatahoarder@noreply.gammaspectra.live>
2025-06-26 06:16:08 +0200

    ci: chdir on monero/block tests

--- monero/block/block_test.go
+++ monero/block/block_test.go
@@ -4,8 +4,20 @@ import (
 	"bytes"
 	"encoding/hex"
 	"os"
+	"path"
+	"runtime"
 	"testing"
 )
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	// The ".." may change depending on you folder structure
+	dir := path.Join(path.Dir(filename), "../..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
 
 var fuzzPoolBlocks = []string{
 	"testdata/v4_block.dat",
