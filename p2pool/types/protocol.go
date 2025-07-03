d55ef6b4c89302b8b6108b11e7046eb73eb30f21
WeebDataHoarder <weebdatahoarder@noreply.gammaspectra.live>
2025-07-03 03:56:41 +0200

    Version v4.3.0

--- p2pool/types/protocol.go
+++ p2pool/types/protocol.go
@@ -128,8 +128,8 @@ func (v SoftwareVersion) SoftwareAwareString(id SoftwareId) string {
 const SupportedProtocolVersion = ProtocolVersion_1_3
 
 const CurrentSoftwareVersionMajor = 4 & 0xFFFF
-const CurrentSoftwareVersionMinor = 2 & 0xFF
-const CurrentSoftwareVersionPatch = 1 & 0xFF
+const CurrentSoftwareVersionMinor = 3 & 0xFF
+const CurrentSoftwareVersionPatch = 0 & 0xFF
 
 const CurrentSoftwareVersion SoftwareVersion = (CurrentSoftwareVersionMajor << 16) | (CurrentSoftwareVersionMinor << 8) | CurrentSoftwareVersionPatch
 const CurrentSoftwareId = SoftwareIdGoObserver
