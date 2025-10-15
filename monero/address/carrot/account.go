8e55194b2c105c5ed04e724697b0a405d55036a7
WeebDataHoarder <weebdatahoarder@noreply.gammaspectra.live>
2025-10-15 17:03:55 +0200

    carrot: add account utilities and tests

---
+++ monero/address/carrot/account.go
@@ -0,0 +1,99 @@
+package carrot
+
+import (
+	"encoding/binary"
+
+	"git.gammaspectra.live/P2Pool/blake2b"
+	"git.gammaspectra.live/P2Pool/consensus/v5/monero/address"
+	"git.gammaspectra.live/P2Pool/consensus/v5/monero/crypto"
+	"git.gammaspectra.live/P2Pool/consensus/v5/types"
+)
+
+// makeProveSpendKey make_carrot_provespend_key
+func makeProveSpendKey(hasher *blake2b.Digest, proveSpendOut *crypto.PrivateKeyScalar, masterSecret types.Hash) {
+	// k_ps = H_n(s_m)
+	ScalarTranscript(
+		proveSpendOut.Scalar(), hasher, masterSecret[:],
+		[]byte(DomainSeparatorProveSpendKey),
+	)
+}
+
+// makeViewBalanceSecret make_carrot_viewbalance_secret
+func makeViewBalanceSecret(hasher *blake2b.Digest, masterSecret types.Hash) (viewBalanceSecret types.Hash) {
+	// s_vb = H_32(s_m)
+	HashedTranscript(
+		viewBalanceSecret[:], hasher, masterSecret[:],
+		[]byte(DomainSeparatorViewBalanceSecret),
+	)
+	return viewBalanceSecret
+}
+
+// makeGenerateImageKey make_carrot_generateimage_key
+func makeGenerateImageKey(hasher *blake2b.Digest, viewIncomingKeyOut *crypto.PrivateKeyScalar, viewBalanceSecret types.Hash) {
+	// k_gi = H_n(s_vb)
+	ScalarTranscript(
+		viewIncomingKeyOut.Scalar(), hasher, viewBalanceSecret[:],
+		[]byte(DomainSeparatorGenerateImageKey),
+	)
+}
+
+// makeViewIncomingKey make_carrot_viewincoming_key
+func makeViewIncomingKey(hasher *blake2b.Digest, viewIncomingKeyOut *crypto.PrivateKeyScalar, viewBalanceSecret types.Hash) {
+	// k_v = H_n(s_vb)
+	ScalarTranscript(
+		viewIncomingKeyOut.Scalar(), hasher, viewBalanceSecret[:],
+		[]byte(DomainSeparatorIncomingViewKey),
+	)
+}
+
+// makeGenerateAddressSecret make_carrot_generateaddress_secret
+func makeGenerateAddressSecret(hasher *blake2b.Digest, viewBalanceSecret types.Hash) (generateAddressSecret types.Hash) {
+	// s_ga = H_32(s_vb)
+	HashedTranscript(
+		generateAddressSecret[:], hasher, viewBalanceSecret[:],
+		[]byte(DomainSeparatorGenerateAddressSecret),
+	)
+	return generateAddressSecret
+}
+
+// makeSpendPub make_carrot_spend_pubkey
+func makeSpendPub(addressSpendPubOut *crypto.PublicKeyPoint, generateImage, proveSpend *crypto.PrivateKeyScalar) {
+	// K_s = k_gi G + k_ps T
+	addressSpendPubOut.Point().UnsafeVarTimeDoubleScalarBaseMultPrecomputed(proveSpend.Scalar(), generatorTPrecomputedTable, generateImage.Scalar())
+}
+
+// makeAccountViewPub make_carrot_account_view_pubkey
+func makeAccountViewPub(addressViewPubOut *crypto.PublicKeyPoint, view *crypto.PrivateKeyScalar, spendPub *crypto.PublicKeyPoint) {
+	addressViewPubOut.Point().UnsafeVarTimeScalarMult(view.Scalar(), spendPub.Point())
+}
+
+// makePrimaryAddressViewPub make_carrot_primary_address_view_pubkey
+func makePrimaryAddressViewPub(addressViewPubOut *crypto.PublicKeyPoint, view *crypto.PrivateKeyScalar) {
+	// K^0_v = k_v G
+	addressViewPubOut.Point().UnsafeVarTimeScalarBaseMult(view.Scalar())
+}
+
+// makeIndexExtensionGenerator make_carrot_index_extension_generator
+func makeIndexExtensionGenerator(hasher *blake2b.Digest, generateAddressSecret types.Hash, i address.SubaddressIndex) (addressIndexGeneratorSecret types.Hash) {
+	// s^j_gen = H_32[s_ga](j_major, j_minor)
+	var buf [8]byte
+	binary.LittleEndian.PutUint32(buf[:], i.Account)
+	binary.LittleEndian.PutUint32(buf[4:], i.Offset)
+	HashedTranscript(
+		addressIndexGeneratorSecret[:], hasher, generateAddressSecret[:],
+		[]byte(DomainSeparatorAddressIndexGenerator), buf[:],
+	)
+	return addressIndexGeneratorSecret
+}
+
+// makeSubaddressScalar make_carrot_subaddress_scalar
+func makeSubaddressScalar(hasher *blake2b.Digest, addressIndexGeneratorSecretOut *crypto.PrivateKeyScalar, spendPub crypto.PublicKeyBytes, addressIndexGeneratorSecret types.Hash, i address.SubaddressIndex) {
+	// k^j_subscal = H_n(K_s, j_major, j_minor, s^j_gen)
+	var buf [8]byte
+	binary.LittleEndian.PutUint32(buf[:], i.Account)
+	binary.LittleEndian.PutUint32(buf[4:], i.Offset)
+	ScalarTranscript(
+		addressIndexGeneratorSecretOut.Scalar(), hasher, addressIndexGeneratorSecret[:],
+		[]byte(DomainSeparatorSubaddressScalar), spendPub[:], buf[:],
+	)
+}
